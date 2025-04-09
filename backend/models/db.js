120f80d2314f33c7d324d99817a3c14b6c319823
siddharth <siddharthtiwari1265@gmail.com>
2025-04-09 18:54:17 +0530

    Ready for deployment

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,17 +1,21 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'system', // if you have a password, add it here
-  database: 'Bank'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
   if (err) {
     console.error('MySQL connection error:', err);
   } else {
-    console.log('Connected to MySQL database ✅');
+    console.log('✅ Connected to MySQL database');
   }
 });
 
